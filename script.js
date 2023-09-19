4ee92363b1730e27a08266ec06f2b0eefc06342e
mouram chafikE <mrmouramito@gmail.com>
2023-09-19 15:24:36 +0200

    fisr commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+ let firstValue  = parseInt(prompt("entre a  first value?"));
+ let operator = prompt("entre type of operation");
+ let secondeValue = parseInt(prompt("entre a secende value?"));
+let result=0
+
+            // Operation +
+if (operator === "+" ) 
+{
+    console.log("Bonjour !")
+    result =  firstValue + secondeValue
+    console.log("The risult is : " + result);
+}
+else 
+{
+    console.log("The risult is : " + result);
+}
+
+            // Operation -
+if (operator === "-" ) 
+{
+    console.log("Bonjour !")
+    result =  firstValue - secondeValue
+    console.log("The risult is : " + result);
+}
+else 
+{
+    console.log("The risult is : " + result);
+}
+
+            // Operation *
+if (operator === "*" ) 
+{
+    console.log("Bonjour !")
+    result =  firstValue * secondeValue
+    console.log("The risult is : " + result);
+}
+else 
+{
+    console.log("The risult is : " + result);
+}
+
+            // Operation / 
+
+if (operator === "/" ) 
+{
+    console.log("Bonjour !")
+    result =  firstValue / secondeValue
+    console.log("The risult is : " + result);
+}
+else 
+{
+    console.log("The risult is : " + result);
+}
