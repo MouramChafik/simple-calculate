7a1613667394e1bd45bcfd43b9e2e9e4bad414bf
mouram chafikE <mrmouramito@gmail.com>
2023-09-19 15:50:02 +0200

    code pref

--- script.js
+++ script.js
@@ -1,54 +1,24 @@
+let firstValue = parseInt(prompt("Entrez la première valeur :"));
+let operator = prompt("Entrez l'opération (+, -, *, /) :");
+let secondeValue = parseInt(prompt("Entrez la deuxième valeur :"));
+let result = 0;
 
- let firstValue  = parseInt(prompt("entre a  first value?"));
- let operator = prompt("entre type of operation");
- let secondeValue = parseInt(prompt("entre a secende value?"));
-let result=0
+console.log("Bonjour !");
 
-            // Operation +
-if (operator === "+" ) 
-{
-    console.log("Bonjour !")
-    result =  firstValue + secondeValue
-    console.log("The risult is : " + result);
-}
-else 
-{
-    console.log("The risult is : " + result);
-}
-
-            // Operation -
-if (operator === "-" ) 
-{
-    console.log("Bonjour !")
-    result =  firstValue - secondeValue
-    console.log("The risult is : " + result);
-}
-else 
-{
-    console.log("The risult is : " + result);
+if (operator === "+") {
+    result = firstValue + secondeValue;
+} else if (operator === "-") {
+    result = firstValue - secondeValue;
+} else if (operator === "*") {
+    result = firstValue * secondeValue;
+} else if (operator === "/") {
+    if (secondeValue === 0) {
+        console.log("Erreur : division par zéro !");
+    } else {
+        result = firstValue / secondeValue;
+    }
+} else {
+    console.log("Opération non reconnue.");
 }
 
-            // Operation *
-if (operator === "*" ) 
-{
-    console.log("Bonjour !")
-    result =  firstValue * secondeValue
-    console.log("The risult is : " + result);
-}
-else 
-{
-    console.log("The risult is : " + result);
-}
-
-            // Operation / 
-
-if (operator === "/" ) 
-{
-    console.log("Bonjour !")
-    result =  firstValue / secondeValue
-    console.log("The risult is : " + result);
-}
-else 
-{
-    console.log("The risult is : " + result);
-}
+console.log("Le résultat est : " + result);
